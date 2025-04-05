fc3c39e243c8d099af38e2aef40dc12ba41cfe8d
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-04-04 20:38:22 -0500

    Completed 2 exercises

--- sample.java
+++ sample.java
@@ -16,5 +16,24 @@ class sample{
         }
     }
     /*****************PROBLEM-2************/
-    
+    //TC:0(n)
+//sc:0(1)
+    class Solution {
+        public int maxProfit(int[] prices) {
+            if(prices.length<=1 || prices==null){
+                return 0;
+            }
+            int buy1=Integer.MAX_VALUE,sell1=0,buy2=Integer.MAX_VALUE,sell2=0;
+            for(int price:prices){
+                buy1=Math.min(buy1,price);
+                sell1=Math.max(sell1,price-buy1);
+                buy2=Math.min(buy2,price-sell1);
+                sell2=Math.max(sell2,price-buy2);
+            }
+            return sell2;
+        }
+    }
+
+    /*****************PROBLEM-3************/
+    /*****************PROBLEM-4************/
 }
